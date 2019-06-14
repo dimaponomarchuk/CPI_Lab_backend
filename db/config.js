8be95affd00212286385227a5c8d3cd4bdf5c6c2
Dima <crazybanana>
2019-06-14 19:27:23 +0300

    Migration bug with config file fixed

--- db/config.js
+++ db/config.js
@@ -1,3 +1,11 @@
+const path = require('path');
+
+const nodeEnv = process.env.NODE_ENV || 'development';
+
+require('dotenv').config({
+    path: path.join(__dirname, '../.env.' + nodeEnv)
+});
+
 module.exports = {
     development: {
         username: process.env.DB_USERNAME,
@@ -5,5 +13,5 @@ module.exports = {
         database: process.env.DB_DATABASE,
         host: process.env.DB_HOST,
         dialect: "mysql"
-    },
+    }
 };
